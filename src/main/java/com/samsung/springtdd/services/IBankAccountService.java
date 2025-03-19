4d9dcab9c1d76aaed2dc8ac1c49d2dcbcc4a64a6
Tuong Tran <tranvantuong2k3@gmail.com>
2025-03-19 11:18:43 +0700

    refactor-code

--- src/main/java/com/samsung/springtdd/services/IBankAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.samsung.springtdd.services;
-
-import com.samsung.springtdd.models.BankAccount;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IBankAccountService {
-    BankAccount drawMoney(String accountNumber, double balance);
-}
